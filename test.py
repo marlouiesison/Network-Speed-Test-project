2a01b95f424b111b7914c7db45e99d5406af8542
Marlouie Sison <marlouiesison@gmail.com>
2022-12-05 15:35:02 -0500

    final code changes

--- test.py
+++ test.py
@@ -1,20 +1,85 @@
+# Python program to test
+# internet speed
+import tkinter as tk
+from tkinter import *
 import speedtest
+import tkinter.messagebox
+from tkinter import ttk
+from tkinter import font
+import threading
+import time
+from multiprocessing import Process
 
-st1 = speedtest.Speedtest() #Create a speedtest object. More info on line 1077
+global speedWithUnits
+speedWithUnits = ''
+option=''
+def downloadSpeed():
+	global option
+	option='Download Speed'
+	showSpeed()
 
-print("Starting Speed test... \n")
+def uploadSpeed():
+	global option
+	option='Upload Speed'
+	showSpeed()
 
-#Test Download
-print("Retrieving download speed...")
-download_speed = st1.download() #Get download speed function. More info on line 1507
-print(f"Download speed: {download_speed} \n")
+def ping():
+	global option
+	option='Ping'
+	showSpeed()
 
-#Test Upload
-print("Retrieving upload speed...")
-upload_speed = st1.upload() #Get download speed function. More info on line 1582
-print(f"Upload speed: {upload_speed}\n")
+def showSpeed():
+	global option
+	st = speedtest.Speedtest()
+	if option == 'Download Speed':
+		speed=(st.download())
+	elif option == 'Upload Speed':
+		speed=(st.upload())
+	elif option == 'Ping':
+		servernames =[]
+		st.get_servers(servernames)
+		Pingspeed =(st.results.ping)
+		speed = -1
+	speedWithUnits=''
+	if(speed == -1):
+		speedWithUnits=str(Pingspeed)+" ms"
+	elif(speed<1000):
+		speedWithUnits=str(round(speed, 3))+" bps"
+	elif(speed<1000000):
+		speedWithUnits=str(round(speed/1000, 3))+" Kbps"
+	elif(speed<1000000000):
+		speedWithUnits=str(round(speed/1000000, 3))+" Mbps"
+	else:
+		speedWithUnits=str(round(speed/1000000000, 3))+" Gbps"
+	tkinter.messagebox.showinfo("Internet Speed Test Tool",  "Hello! Your " +option+" Speed is:"+speedWithUnits)
 
-#Test Ping
-print("Retrieving ping(latency)...") #Test Latency from nearest server by default. More info on line 1411
-ping = st1.results.ping
-print(f"Ping(Latency) {ping}")
+#Creating the main window 
+wn = tkinter.Tk() 
+wn.title("Network Speed Test Tool")
+wn.geometry('1300x750')
+
+#add background image
+bg = PhotoImage(file = "professor.png")
+label1 = Label( wn, image = bg)
+label1.place(x = 0, y = 0, relwidth=1, relheight=1)
+
+Label(wn, text='Network Speed Test Tool(ECE422)',bg='ivory3',
+      fg='black', font=('times new roman', 28)).place(x=120, y=80, anchor=NW)
+
+Label(wn, text='Choose any of the below options',bg='ivory3',
+      fg='black', font=('times new roman', 18)).place(x=230, y=135)
+
+#Button to Check Download Speed
+Button(wn, text="Test Download Speed", bg='ivory3',font=('times new roman', 15),width=25,height=2,
+       command=downloadSpeed).place(x=240, y=200)
+
+#Button to Check Upload Speed
+Button(wn, text="Test Upload Speed", bg='ivory3',font=('times new roman', 15),width=25,height=2,
+       command=uploadSpeed).place(x=240, y=270)
+	   
+#Button to convert Audio to PDF form
+Button(wn, text="Test Ping", bg='ivory3',font=('times new roman', 15),width=25,height=2,
+       command=ping).place(x=240, y=340)
+
+#Runs the window till it is closed
+wn.mainloop()
